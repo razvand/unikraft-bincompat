4a430e4bb4c818e9ffb70df176d5451de7ecc3c2
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2022-05-13 00:34:49 +0300

    lib/posix-info: Comment out unused sysinfo buffer
    The sysinfo structure defines the `reserved` buffer. This is however not
    present on Linux, meaning that binary-compatibile applications use a
    shorter buffer. This results in a buffer overflow when transfering
    information from Unikraft to the buffer in binary-compatible app.
    
    This commit fixes this by commenting out the `reserved` buffer in the
    `sysinfo` structure.
    
    Signed-off-by: Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>

--- lib/posix-sysinfo/include/sys/sysinfo.h
+++ lib/posix-sysinfo/include/sys/sysinfo.h
@@ -46,7 +46,7 @@ struct sysinfo {
 	unsigned long totalhigh;
 	unsigned long freehigh;
 	unsigned mem_unit;
-	char __reserved[256];
+	char _f[20-2*sizeof(unsigned long)-sizeof(unsigned)];
 };
 
 int sysinfo (struct sysinfo *);
